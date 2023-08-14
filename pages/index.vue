96032fbf38b8202f81c9cd77509487b09d5bd56c
Muhammad Umar Mansyur <umar.ovie@gmail.com>
2023-08-15 06:20:53 +0700

    pupsh dulu

--- pages/index.vue
+++ pages/index.vue
@@ -251,38 +251,34 @@
             <div class="col-12 my-5" id="pricing">
               <h3 class="text-center">Pricing</h3>
             </div>
-            <div v-if="categories">
-              <div v-if="categories.data">
-                <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 mb-3" data-aos="fade-up" v-for="item in categories.data.data" v-if="categories.data">
-                  <div class="single__service">
-                    <div class="service__img">
-                      <div class="service__bg__img">
-                        <svg class="service__icon__bg" fill="none" xmlns="http://www.w3.org/2000/svg">
-                          <path fill-rule="evenodd" clip-rule="evenodd"
-                            d="M63.3775 44.4535C54.8582 58.717 39.1005 53.2202 23.1736 47.5697C7.2467 41.9192 -5.18037 32.7111 3.33895 18.4477C11.8583 4.18418 31.6595 -2.79441 47.5803 2.85105C63.5011 8.49652 71.8609 30.2313 63.3488 44.4865L63.3775 44.4535Z"
-                            fill="#5F2DED" fill-opacity="0.05" />
-                        </svg>
-                      </div>
-                    </div>
-                    <div class="service__content">
-                      <h2>{{  item.name }}</h2>
-                      <span>Setoran Perbulan (1 Tahun): </span>
-                      <h4>{{ new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(item.limit / 12).slice(0,-3)  }}</h4>
-                      <span>Setoran Perbulan (2 Tahun): </span>
-                      <h4>{{ new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(item.limit / 24).slice(0,-3)  }}</h4>
-                      <span>Setoran Perbulan (3 Tahun): </span>
-                      <h4>{{ new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(item.limit / 36).slice(0,-3)  }}</h4>
-                      <span>Limit Tabungan: </span>
-                      <h4>{{ new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(item.limit).slice(0,-3) }}</h4>
-                    </div>
-                    <div class="service__small__img">
-                      <svg class="icon__hover__img" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path
-                          d="M16.5961 10.265L19 1.33069L10.0022 3.73285L1 6.1306L7.59393 12.6627L14.1879 19.1992L16.5961 10.265Z"
-                          stroke="#FFB31F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                      </svg>
-                    </div>
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 mb-3" data-aos="fade-up" v-for="item in categories.data.data">
+              <div class="single__service">
+                <div class="service__img">
+                  <div class="service__bg__img">
+                    <svg class="service__icon__bg" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path fill-rule="evenodd" clip-rule="evenodd"
+                        d="M63.3775 44.4535C54.8582 58.717 39.1005 53.2202 23.1736 47.5697C7.2467 41.9192 -5.18037 32.7111 3.33895 18.4477C11.8583 4.18418 31.6595 -2.79441 47.5803 2.85105C63.5011 8.49652 71.8609 30.2313 63.3488 44.4865L63.3775 44.4535Z"
+                        fill="#5F2DED" fill-opacity="0.05" />
+                    </svg>
                   </div>
+                </div>
+                <div class="service__content">
+                  <h2>{{  item.name }}</h2>
+                  <span>Setoran Perbulan (1 Tahun): </span>
+                  <h4>{{ new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(item.limit / 12).slice(0,-3)  }}</h4>
+                  <span>Setoran Perbulan (2 Tahun): </span>
+                  <h4>{{ new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(item.limit / 24).slice(0,-3)  }}</h4>
+                  <span>Setoran Perbulan (3 Tahun): </span>
+                  <h4>{{ new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(item.limit / 36).slice(0,-3)  }}</h4>
+                  <span>Limit Tabungan: </span>
+                  <h4>{{ new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(item.limit).slice(0,-3) }}</h4>
+                </div>
+                <div class="service__small__img">
+                  <svg class="icon__hover__img" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M16.5961 10.265L19 1.33069L10.0022 3.73285L1 6.1306L7.59393 12.6627L14.1879 19.1992L16.5961 10.265Z"
+                      stroke="#FFB31F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                  </svg>
                 </div>
               </div>
             </div>
@@ -358,7 +354,10 @@
 
 <script setup lang="ts">
 
-const { data: categories }:any = await useFetch('https://api.simola.kampungmelonnapote.co.id/saving-categories');
+const { data: categories }:any = await useFetch('https://api.simola.kampungmelonnapote.co.id/saving-categories', {
+  lazy: true,
+  server: true
+});
 const urllogin = 'https://simola.back.pembangunanpamekasan.com/login';
 const urlregistrasi = 'https://simola.back.pembangunanpamekasan.com/register';
 
